1c0986e6e7dddacc0eca4f2a6df0fd3635925529
Nash Pillai <nash.pillai@protonmail.com>
2024-08-03 19:59:27 -0400

    import differently

--- dist/index.js
+++ dist/index.js
@@ -6,13 +6,33 @@
 
 "use strict";
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(9093));
+const core = __importStar(__nccwpck_require__(9093));
 try {
-    const secretsJson = core_1.default.getInput("secrets", { required: true });
+    const secretsJson = core.getInput("secrets", { required: true });
     let secrets;
     try {
         secrets = JSON.parse(secretsJson);
@@ -21,13 +41,13 @@ try {
         throw new Error(`Cannot parse JSON secrets`);
     }
     for (const [key, value] of Object.entries(secrets)) {
-        core_1.default.exportVariable(key, value);
-        core_1.default.info(`Exported ${key}`);
+        core.exportVariable(key, value);
+        core.info(`Exported ${key}`);
     }
 }
 catch (error) {
     if (error instanceof Error)
-        core_1.default.setFailed(error.message);
+        core.setFailed(error.message);
 }
 
 
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import core from "@actions/core";
+import * as core from "@actions/core";
 
 try {
 	const secretsJson = core.getInput("secrets", { required: true });
